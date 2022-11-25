5111b9f4d7caf8627ad699cd86da29107f469fe3
Chandana <chandanasuresh2004@gmail.com>
2022-11-25 12:08:41 +0530

    demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemComponentComponent } from './item-component/item-component.component';
+import { ItemListComponentComponent } from './item-list-component/item-list-component.component';
+import { CartComponentComponent } from './cart-component/cart-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemComponentComponent,
+    ItemListComponentComponent,
+    CartComponentComponent
   ],
   imports: [
     BrowserModule,
